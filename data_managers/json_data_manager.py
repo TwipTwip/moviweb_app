a78705bae4fb73efa241eda409c57420ae931802
Dylan Harding <dmharding34@gmail.com>
2023-07-10 21:50:46 -0600

    Final Product

--- data_managers/json_data_manager.py
+++ data_managers/json_data_manager.py
@@ -2,20 +2,6 @@ import json
 from moviweb_app.data_managers.data_manager_interface import DataManagerInterface
 
 
-# data = [
-#     {1: {"name": "Alice", "movies": {"id": 4,
-#                                      "director": "somebody",
-#                                      "title": "movie title",
-#                                      "year": 2004}}},
-#     {2: {"name": "Bob", "movies": {"id": 4,
-#                                    "director": "somebody",
-#                                    "title": "movie title",
-#                                    "year": 2004}}}
-# ]
-# with open("users.json", "w") as rewrite:
-#     json.dump(data, rewrite)
-
-
 class JSONDataManager(DataManagerInterface):
     def __init__(self, filename):
         self.filename = filename
@@ -30,12 +16,6 @@ class JSONDataManager(DataManagerInterface):
         id_counter = 0
         str_counter = 0
         for user in users:
-            # id_counter += 1
-            # counter += 1
-            # str_counter = int(str_counter)
-            # str_counter += 1
-            # str_counter = str(str_counter)
-            # movie_data = {"id": id_counter, "name": users[counter][str_counter]['name']}
             all_movie_info.append(user)
         return all_movie_info
 
@@ -49,9 +29,6 @@ class JSONDataManager(DataManagerInterface):
                 counter += 1
                 if str(user_id) == str(key):
                     user_movies = users[counter][key]['movies']
-                    # for movie in user_movies:
-                    #     print(f"{movie['title']}({movie['year']}), "
-                    #           f"Director: {movie['director']}, Movie ID: {movie['id']}")
         return user_movies
 
     def get_username_by_id(self, user_id):
@@ -64,6 +41,3 @@ class JSONDataManager(DataManagerInterface):
                     return user_name
         return None
 
-
-# JSONDataManager("users.json").get_all_users()
-# JSONDataManager("../storage/users.json").get_user_movies(1)
